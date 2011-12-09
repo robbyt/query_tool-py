f9d64d7e8f651930a6946026420f9573f8d5630c
Rob Terhaar <rob@atlanticdynamic.com>
2011-12-09 23:39:48 +0000

    adding a TBD exception, for some things that need to be implemented...

--- query_tool/query_tool.py
+++ query_tool/query_tool.py
@@ -2,41 +2,51 @@
 import sys
 from user_input import UserInput
 from curl_actions import CurlActions
+from output_actions import OutputActions
+
+class Tbd(Exception):
+    pass
 
 if __name__ == '__main__':
     # parse the args
     ui = UserInput(sys.argv[1:])
 
-    # collect the parsed args into a dict
-    data = ui.get_args_as_dict()
-
     # throw that dict at the curlactions class to get our data
-    action = CurlActions(facts = ui.get_facts_as_dict(), **data)
+    action = CurlActions()
+
+    # perform the query
     action.run()
 
+    # throw the output from that query into the output parser
+    output = OutputActions(action.return_yaml())
+
+    if output.default_output():
+        print output
+    else:
+        raise Tbd('not done yet')
+
+    """
+    ### end ###
     # if the output format is default, just output the FQDNs
-    if data['output_fact'] == 'fqdn':
-        if data['debug']: print 'output_fact is set to "%s" so we can return the current data' % (data['output_fact'])
         
-        if data['yaml']:
+        if ui.data['yaml']:
             print action.return_yaml()
         else:
-            print action.return_text()
+            print y.to_text(action.return_yaml())
 
     # otherwise, we have to do some more work
     else:
-        if data['debug']: print 'output_fact is set to "%s" so we have to run some more queries' % (data['output_fact'])
         search_results = {}
-        for t in action.return_list():
+        for t in y.to_list(action.return_yaml()):
             node_search = CurlActions(target=t, fact_search=False, **data)
             node_search.run()
-            search_results[t] = str(node_search.return_text())
+            search_results[t] = y.to_text(node_search.return_yaml())
             if data['debug']: print 'Added a new element to search_results: %s' % (search_results[t])
 
         if data['yaml']:
             print 'not implemented.'
         else:
-            print search_results
-            #'\n'.join(search_results)
+            for i in search_results.iteritems():
+                print i
         
-        
+    """ 
