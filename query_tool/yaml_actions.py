1e97d26fb8edafe3352be2d66e3a5b0f68d95fbf
Rob Terhaar <rob@atlanticdynamic.com>
2011-12-11 02:38:11 +0000

    cleanup

---
+++ query_tool/yaml_actions.py
@@ -0,0 +1,31 @@
+from yaml import load, add_constructor
+#from yaml import dump
+from user_input import UserInput as ui
+
+#try:
+#    from yaml import CLoader as Loader, CDumper as Dumper
+#except ImportError:
+#    from yaml import Loader, Dumper
+
+class YamlActions(object):
+
+    def __init__(self):
+        self._yaml_hack()
+
+    def _yaml_builder(self, loader, node):
+        if ui.debug: print "Building some yaml"
+        return loader.construct_mapping(node)['values']
+
+    def _yaml_hack(self):
+        if ui.debug: print "Adding the yaml hack"
+        add_constructor(u'!ruby/object:Puppet::Node::Facts', self._yaml_builder)
+
+    def to_text(self, yaml_string):
+        if ui.debug: print "returning text"
+        t = load(yaml_string)
+        return '\n'.join(t)
+
+    def to_list(self, yaml_string):
+        if ui.debug: print "returning a python list object"
+        return load(yaml_string)
+
